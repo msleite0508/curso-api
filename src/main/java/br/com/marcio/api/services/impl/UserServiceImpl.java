95817091a86932a50728de00ff2953c0e453e504
msleite0508 <marcioleite09@gmail.com>
2023-01-08 11:13:45 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/marcio/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package br.com.marcio.api.services.impl;
+
+import br.com.marcio.api.domain.User;
+import br.com.marcio.api.repositories.UserRepository;
+import br.com.marcio.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
