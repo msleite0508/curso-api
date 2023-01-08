d6426547ff282a4073fe70d05aaee9c87f292938
msleite0508 <marcioleite09@gmail.com>
2023-01-08 11:13:10 -0300

    interface user repository

---
+++ src/main/java/br/com/marcio/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.marcio.api.repositories;
+
+import br.com.marcio.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
